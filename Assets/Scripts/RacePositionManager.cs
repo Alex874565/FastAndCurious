a703f045ed503bc98e7207fcc3388c14a203b462
Alex874565 <alex.carpinisan@gmail.com>
2025-06-09 20:33:00 +0300

    Fix?

--- Assets/Scripts/RacePositionManager.cs
+++ Assets/Scripts/RacePositionManager.cs
@@ -7,9 +7,33 @@ public class RacePositionManager : MonoBehaviour
     public static RacePositionManager Instance;
     private List<PlayerDistanceTracker> players = new List<PlayerDistanceTracker>();
 
+    [SerializeField] TMPro.TMP_Text positionText;
+
     void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Instance != null) {
+            if (positionText != null)
+            {
+                if (players.Count > 0)
+                {
+                    var myTracker = players.FirstOrDefault(p => p.photonView.IsMine);
+                    if (myTracker != null)
+                    {
+                        int position = GetPlayerPosition(myTracker);
+                        positionText.text = $"Position: {position}";
+                    }
+                }
+                else
+                {
+                    positionText.text = "No players registered.";
+                }
+            }
+        }
     }
 
     public void RegisterPlayer(PlayerDistanceTracker tracker)
@@ -28,4 +52,5 @@ public class RacePositionManager : MonoBehaviour
         var sorted = players.OrderByDescending(p => p.DistanceTravelled).ToList();
         return sorted.IndexOf(myTracker) + 1;
     }
+
 }
